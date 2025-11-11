55af28c252f53c3544f19075fa6fcbcf0144313b
daniell escareno <daniellescareno7@gmail.com>
2025-11-11 07:57:35 -0800

    Initial Commit

---
+++ Tic Tac toe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tic Tac toe
+//
+//  Created by daniell Escareno on 11/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tic Tac toe/Tic_Tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_toeApp.swift
+//  Tic Tac toe
+//
+//  Created by daniell Escareno on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
